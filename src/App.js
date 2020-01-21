8c01d8c620b1d1bdd5fa138f9e1092bbe404dae6
Prajakta Kathe <prajakta.kathe@inscripts.in>
2020-01-21 20:07:14 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { data,Row, Col} from "../src/resources/data";
+import Section from './components/Section';
+import Card from './components/Card';
+import Tag from './components/Tag';
+import Markdown from './components/Markdown';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  
+  
+    render() {
+    return (
+      <div className="App">
+        {getData(data)}
+      </div>
+    );
+  }
+
+  
 }
 
 export default App;
+
+
+export const getData=(data)=>{  
+  if(data instanceof Array)
+  return data.map((component,key)=>{
+     switch(component.component){
+       case "Section":{
+         return <Section {...component.props} key={key} ></Section>          
+       }        
+       case "Row":{
+         return <Row {...component.props}key={key} ></Row>
+        
+       }
+       case "Col":{
+         return <Col {...component.props} key={key} ></Col>   
+       }        
+       
+       case "Markdown":{
+         return <Markdown {...component.props}key={key} ></Markdown>
+        
+       }
+       case "Tag":{
+         return <Tag {...component.props}key={key} ></Tag>
+        
+       }
+       case "Card":{
+         return <Card {...component.props}key={key} ></Card>
+        
+       } 
+        default:
+          break;
+     }
+     return true
+   });
+   else if(typeof(data)==="string"){
+    return <div>{data}</div>
+   }   
+ }
